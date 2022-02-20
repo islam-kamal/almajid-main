5f9c576c3f41e1e1c87a175ba903d73954affbfc
Mohamed Abdelrasoul <mohammed.abdelrasoul@gmail.com>
2022-02-20 11:04:14 +0300

    adding chanes to apple pay

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -2,17 +2,17 @@ import 'package:almajidoud/main.dart';
 import 'package:almajidoud/utils/file_export.dart';
 
 class Urls {
-/*
+
  // test base url
   static final String BASE_URL = "https://test.almajed4oud.com";
   // //test admin token
   static final String ADMIN_TOKEN = 'tda5h42j6mke2q43da55wckmoeynz1n1';
 
-*/
+
   //production base url
-  static final String BASE_URL = "https://almajed4oud.com";
+  //static final String BASE_URL = "https://almajed4oud.com";
   //production admin token
-  static final String ADMIN_TOKEN = 'akywvzb51qazebzlzll0nutx3zrry7ao';
+  //static final String ADMIN_TOKEN = 'akywvzb51qazebzlzll0nutx3zrry7ao';
 
   static final String SIGN_UP = "/${MyApp.app_langauge}-${MyApp.app_location}/index.php/rest/V1/mobilelogin/account/create";
 
